f9e2bc998e923e092af6af357a4ab66f00099887
Natasha <nta.13@live.com>
2025-04-22 17:00:20 -0300

    initial structure gradle

---
+++ src/main/java/br/com/library/data_book/DataBookApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.library.data_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/library/data_book/DataBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.library.data_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
